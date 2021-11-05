f7d9366a2eb04f08b9d62b58a4c50800d5e0c13b
golddong98 <73287240+golddong98@users.noreply.github.com>
2021-11-05 22:49:28 +0900

    (진)완성

--- static/index.js
+++ static/index.js
@@ -1,62 +1,84 @@
 //동영상 좋아요 색상변경 - 블루
+const twinkle = document.querySelector("#twinkle");
+twinkle.addEventListener("click", onoff);
+
 function like_color_change_blue() {
-  document.getElementById('like_color_change').style.color = '#3ea6ff'
-  document.getElementById('like_color_change').style.transition = '0.35s'
-  document.getElementById('like_color_change_2').style.color = '#3ea6ff'
-  document.getElementById('like_color_change_2').style.transition = '0.35s'
+  document.getElementById("like_color_change").style.color = "#3ea6ff";
+  document.getElementById("like_color_change").style.transition = "0.35s";
+  document.getElementById("like_color_change_2").style.color = "#3ea6ff";
+  document.getElementById("like_color_change_2").style.transition = "0.35s";
 }
 
 //동영상 싫어요 색상변경 - 레드
 function like_color_change_red() {
-  document.getElementById('dislike_color_change').style.color = '#fa4e4e'
-  document.getElementById('dislike_color_change').style.transition = '0.35s'
-  document.getElementById('dislike_color_change_2').style.color = '#fa4e4e'
-  document.getElementById('dislike_color_change_2').style.transition = '0.35s'
+  document.getElementById("dislike_color_change").style.color = "#fa4e4e";
+  document.getElementById("dislike_color_change").style.transition = "0.35s";
+  document.getElementById("dislike_color_change_2").style.color = "#fa4e4e";
+  document.getElementById("dislike_color_change_2").style.transition = "0.35s";
 }
 
-function onoff() {
+function onoff({ target }) {
+  if (target.classList.contains("uuu")) {
+    const { sheet } = Object.assign(
+      document.head.appendChild(document.createElement("style")),
+      { type: "text/css" }
+    );
+    const placeholderStyle =
+      sheet.rules[sheet.insertRule("::placeholder {}")].style;
+    placeholderStyle.color = "#ffffff";
 
-  const {sheet} = Object.assign(document.head.appendChild(document.createElement("style")), {type: "text/css" });
-  const placeholderStyle = sheet.rules[sheet.insertRule("::placeholder {}")].style;
-  placeholderStyle.color = "#ffffff";
+    document.getElementsByClassName("header")[0].style.backgroundColor =
+      "#171717";
+    document.getElementsByClassName("header")[0].style.borderBottom =
+      "1px solid #ffffff";
+    document.getElementsByClassName("header")[0].style.transition = "all 0.3s";
 
-  document.getElementsByClassName('header')[0].style.backgroundColor = '#171717'
-  document.getElementsByClassName('header')[0].style.borderBottom = '1px solid #ffffff'
-  document.getElementsByClassName('header')[0].style.transition = 'all 0.3s'
+    document.getElementsByClassName("user_link_text")[0].style.backgroundColor =
+      "#232323";
+    document.getElementsByClassName(
+      "user_link_button"
+    )[0].style.backgroundColor = "#232323";
+    document.getElementsByClassName("withdrawal")[0].style.backgroundColor =
+      "#232323";
+    document.getElementsByClassName("logout_button")[0].style.backgroundColor =
+      "#232323";
 
-  document.getElementsByClassName('user_link_text')[0].style.backgroundColor = '#232323'
-  document.getElementsByClassName('user_link_button')[0].style.backgroundColor = '#232323'
-  document.getElementsByClassName('withdrawal')[0].style.backgroundColor = '#232323'
-  document.getElementsByClassName('logout_button')[0].style.backgroundColor = '#232323'
-  
-  document.getElementsByClassName('user_link_button')[0].style.color = '#ffffff'
-  document.getElementsByClassName('withdrawal')[0].style.color = '#ffffff'
-  document.getElementsByClassName('logout_button')[0].style.color = '#ffffff'
+    document.getElementsByClassName("user_link_button")[0].style.color =
+      "#ffffff";
+    document.getElementsByClassName("withdrawal")[0].style.color = "#ffffff";
+    document.getElementsByClassName("logout_button")[0].style.color = "#ffffff";
 
-  document.getElementsByClassName('name_text')[0].style.color = '#ffffff'
-  document.getElementsByClassName('youtuber_name_text')[0].style.color = '#ffffff'
-  document.getElementsByClassName('user_comment')[0].style.backgroundColor = '#171717'
-  document.getElementsByClassName('user_comment')[0].style.transition = 'all 0.3s'
-  document.getElementsByClassName('user_comment')[0].style.color = '#ffffff'
+    document.getElementsByClassName("name_text")[0].style.color = "#ffffff";
+    document.getElementsByClassName("youtuber_name_text")[0].style.color =
+      "#ffffff";
+    document.getElementsByClassName("user_comment")[0].style.backgroundColor =
+      "#171717";
+    document.getElementsByClassName("user_comment")[0].style.transition =
+      "all 0.3s";
+    document.getElementsByClassName("user_comment")[0].style.color = "#ffffff";
 
-  document.getElementsByClassName('left_box')[0].style.backgroundColor = '#171717'
-  document.getElementsByClassName('left_box')[0].style.transition = 'all 0.3s'
+    document.getElementsByClassName("left_box")[0].style.backgroundColor =
+      "#171717";
+    document.getElementsByClassName("left_box")[0].style.transition =
+      "all 0.3s";
 
-  document.getElementsByClassName('right_box')[0].style.backgroundColor = '#171717'
-  document.getElementsByClassName('right_box')[0].style.transition = 'all 0.3s'
+    document.getElementsByClassName("right_box")[0].style.backgroundColor =
+      "#171717";
+    document.getElementsByClassName("right_box")[0].style.transition =
+      "all 0.3s";
 
+    document.getElementsByClassName("video_play")[0].style.boxShadow =
+      "0px 8px 12px #333333";
+    document.getElementsByClassName("chat_output")[0].style.boxShadow =
+      "0px 5px 12px #333333";
+    document.getElementsByClassName("chat_rank")[0].style.boxShadow =
+      "0px 5px 12px #333333";
 
-  document.getElementsByClassName('video_play')[0].style.boxShadow = '0px 8px 12px #333333'
-  document.getElementsByClassName('chat_output')[0].style.boxShadow = '0px 5px 12px #333333'
-  document.getElementsByClassName('chat_rank')[0].style.boxShadow = '0px 5px 12px #333333'
+    document.getElementsByClassName("chat_rank")[0].style.color = "#ffffff";
+    document.getElementsByClassName("chat_output")[0].style.color = "#ffffff";
 
-  document.getElementsByClassName('chat_rank')[0].style.color = '#ffffff'
-  document.getElementsByClassName('chat_output')[0].style.color = '#ffffff'
-  
- document.getElementsByClassName('logo')[0].style.display = 'none'
- document.getElementsByClassName('logo_2')[0].style.display = 'block'
- document.getElementsByClassName('logo_2')[1].style.transition = '0.3s'
-
+    document.getElementsByClassName("logo")[0].style.display = "none";
+    document.getElementsByClassName("logo_2")[0].style.display = "block";
+    document.getElementsByClassName("logo_2")[1].style.transition = "0.3s";
+  }
 }
-
-
--- static/video.js
+++ static/video.js
@@ -1,84 +1,77 @@
-const nextVideo = document.querySelector('#next_video')
-const leftBox = document.querySelector('#left_box')
-const urlBtn = document.querySelector('#url-button')
-nextVideo.addEventListener('click', videoLoad)
-urlBtn.addEventListener('click', videoSave)
+const nextVideo = document.querySelector("#next_video");
+const leftBox = document.querySelector("#left_box");
+const urlBtn = document.querySelector("#url-button");
+nextVideo.addEventListener("click", videoLoad);
+urlBtn.addEventListener("click", videoSave);
 let videoThumbnail;
-
 
 // const firstVideoThumbnail = document.querySelector('#first_thumbnail')
 // console.log(firstVideoThumbnail)
 
-window.addEventListener('load', (event) => {
-  videoLoad()
-
-})
+window.addEventListener("load", (event) => {
+  videoLoad();
+});
 
 async function videoSave() {
-    const urlInput = document.querySelector('#url-input')
-    const videoUrl = urlInput.value
-    urlInput.value = ''
-    const options = {
+  const urlInput = document.querySelector("#url-input");
+  const videoUrl = urlInput.value;
+  urlInput.value = "";
+  const options = {
     method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     },
-    body: `videoUrl=${videoUrl}`
+    body: `videoUrl=${videoUrl}`,
   };
   const response = await fetch("/api/video/save", options);
   const result = await response.json();
   const msg = await result["msg"];
-  console.log(msg)
-
+  console.log(msg);
 }
 
 async function videoLoad() {
-    const options = {
+  const options = {
     method: "get",
   };
   const response = await fetch("/api/video/load", options);
   const result = await response.json();
-  const videoTitle = result['videoTitle']
-  const embedUrl = result['embedUrl']
-  videoThumbnail = result['videoThumbnail']
-  const videoUrl = result['videoUrl']
-  const youtuber = result['youtuber']
-    const temp_html= `
-    <div class=“video_play”>
-        <iframe width=“100%” height=“100%” src="${embedUrl}" title=“YouTube video player” frameborder=“0" allow=“accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture” allowfullscreen></iframe>
+  const videoTitle = result["videoTitle"];
+  const embedUrl = result["embedUrl"];
+  videoThumbnail = result["videoThumbnail"];
+  const videoUrl = result["videoUrl"];
+  const youtuber = result["youtuber"];
+  const temp_html = `
+    <div class="video_play">
+        <iframe width="100%" height="100%" src="${embedUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class=“video_information”>
-        <div class=“info_1”>
-          <p class=“rank_text”>:교차된_검: 분노의 투기장 - ARENA of ANGER :교차된_검:</p>
-          <p class=“name_text”>${videoTitle}</p>
-          <p class=“views_text”>조회수 554,905회 2021. 10. 30.</p>
+      <div class="video_information">
+        <div class="info_1">
+          <p class="rank_text">:교차된_검: 분노의 투기장 - ARENA of ANGER :교차된_검:</p>
+          <p class="name_text">${videoTitle}</p>
+          <p class="views_text">조회수 554,905회 2021. 10. 30.</p>
         </div>
-        <div class=“info_2”>
-          <div class=“youtuber_pic”></div>
-          <div class=“youtuber_info”>
-            <p class=“youtuber_name_text”>${youtuber}</p>
-            <p class=“subscriber_number”>구독자 127만명</p>
+        <div class="info_2">
+          <div class="youtuber_pic"></div>
+          <div class="youtuber_info">
+            <p class="youtuber_name_text">${youtuber}</p>
+            <p class="subscriber_number">구독자 127만명</p>
           </div>
         </div>
-        <div class=“info_3">
-          <div class=“like” >
-            <i class=“far fa-thumbs-up”  id=“like_color_change”></i>
-            <p class=“info_like_number” id=“like_color_change_2">
+        <div class="info_3">
+          <div class="like" >
+            <i class="far fa-thumbs-up"  id="like_color_change"></i>
+            <p class="info_like_number" id="like_color_change_2">
               230,4058
             </p>
           </div>
-          <div class=“dislike” >
-            <i class=“far fa-thumbs-down” id=“dislike_color_change”></i>
-            <p class=“info_like_number” id=“dislike_color_change_2">
+          <div class="dislike" >
+            <i class="far fa-thumbs-down" id="dislike_color_change"></i>
+            <p class="info_like_number" id="dislike_color_change_2">
               3058
             </p>
           </div>
         </div>
       </div>
-    `
-    leftBox.innerHTML=`${temp_html}`
-
-
-
+    `;
+  leftBox.innerHTML = `${temp_html}`;
 }
-
